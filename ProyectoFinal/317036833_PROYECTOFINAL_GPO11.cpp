2266c768b737e8d153442a3ae5a9995042f22cad
EyelessJack128 <edu.ortega128@gmail.com>
2023-06-01 09:22:37 -0600

    Habitat Leones
    Se agrego el habitat de leones a OpenGL nadamas falta modelar plantas en el mismo

--- ProyectoFinal/317036833_PROYECTOFINAL_GPO11.cpp
+++ ProyectoFinal/317036833_PROYECTOFINAL_GPO11.cpp
@@ -60,6 +60,8 @@ GLfloat lastFrame = 0.0f;  	// Time of last frame
 
 // Keyframes
 float posX =PosIni.x, posY = PosIni.y, posZ = PosIni.z, rotRodIzq = 0;
+float posXLeones = PosIni.x + 20.0f, posYLeones = PosIni.y, posZLeones = PosIni.z + 20.0f;
+float posXPanda = PosIni.x - 20.0f, posYPanda = PosIni.y, posZPanda = PosIni.z - 20.0f;
 
 #define MAX_FRAMES 9
 int i_max_steps = 190;
@@ -195,7 +197,7 @@ int main()
 	Shader SkyBoxshader("Shaders/SkyBox.vs", "Shaders/SkyBox.frag");
 	Shader animShader("Shaders/anim.vs", "Shaders/anim.frag");
 
-	Model domo((char*)"Models/Domo/domo.obj");
+	/*Model domo((char*)"Models/Domo/domo.obj");
 	Model piso((char*)"Models/Domo/entrada.obj");
 	Model entrada((char*)"Models/Domo/piso.obj");
 	Model cristales((char*)"Models/Domo/cristales.obj");
@@ -208,7 +210,19 @@ int main()
 	Model banca((char*)"Models/Bank/Bank.obj");
 	Model arbol((char*)"Models/tree/tree_maple.obj");
 	Model cuerpoMari((char*)"Models/Butterfly_OBJ2/butterfly_free.obj");
-	Model alas((char*)"Models/Butterfly_OBJ2/wings.obj");
+	Model alas((char*)"Models/Butterfly_OBJ2/wings.obj");*/
+
+	//Lion habitat model declarations
+	Model lionFloor((char*)"Models/HabitatLeones/FloorModel/FloorPlane.obj");
+	Model lionRocks((char*)"Models/HabitatLeones/RockCollection/Rocks.obj");
+	Model lionWater((char*)"Models/HabitatLeones/WaterLakeModel/WaterPlane.obj");
+
+	//Panda habitat model declaration
+	Model pandaFloor((char*)"Models/HabitatPanda/GrassPlane/GrassPlane.obj");
+	Model pandaRocks((char*)"Models/HabitatPanda/Rocks/Rock.obj");
+	Model pandaFence((char*)"Models/HabitatPanda/FenceModel/FenceModel.obj");
+	Model pandaChineseBuilding((char*)"Models/HabitatPanda/ChineseBuilding/ChineseBuilding.obj");
+	Model pandaBambuPlant((char*)"Models/HabitatPanda/BambuPlant/BambuPlant.obj");
 	
 
 	// Build and compile our shader program
@@ -450,7 +464,7 @@ int main()
 		// Point light 1
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].position"), pointLightPositions[0].x, pointLightPositions[0].y, pointLightPositions[0].z);
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].ambient"), 0.3f, 0.3f, 0.3f);
-		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].diffuse"), 0.5f, 0.5f,0.5f);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].diffuse"), 0.5f, 0.5f, 0.5f);
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[0].specular"), 0.8f, 0.8f, 0.8f);
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[0].constant"), 1.0f);
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[0].linear"), 0.09f);
@@ -530,9 +544,10 @@ int main()
 		//Carga de modelo 
 		//Personaje
 
-		
+
 		view = camera.GetViewMatrix();
 		glm::mat4 model(1);
+		/*
 		model = glm::translate(model,glm::vec3(posX,posY,posZ));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		entrada.Draw(lightingShader);
@@ -573,33 +588,57 @@ int main()
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		estructura.Draw(lightingShader);
 
-		/*model = glm::mat4(1);
+		model = glm::mat4(1);
 		model = glm::translate(model, glm::vec3(posX, posY, posZ));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		banca.Draw(lightingShader);*/
+		banca.Draw(lightingShader);
 
 		model = glm::mat4(1);
 		model = glm::translate(model, glm::vec3(posX, posY, posZ));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		cuerpoMari.Draw(lightingShader);
 
-		/*model = glm::mat4(1);
+		model = glm::mat4(1);
 		model = glm::translate(model, glm::vec3(posX, posY, posZ));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 1);
-		arbol.Draw(lightingShader);*/
+		arbol.Draw(lightingShader);
 
 		model = glm::mat4(1);
 		model = glm::translate(model, glm::vec3(posX, posY, posZ));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 1);
-		alas.Draw(lightingShader);
+		alas.Draw(lightingShader);*/
+
+		//Lion Habitat Load
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(posXLeones, posYLeones, posZLeones));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0.0);
+		lionRocks.Draw(lightingShader);
+		lionFloor.Draw(lightingShader);
+
+		//Panda habitat Load
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(posXPanda, posYPanda, posZPanda));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0.0);
+		pandaFloor.Draw(lightingShader);
+		pandaFence.Draw(lightingShader);
+		pandaChineseBuilding.Draw(lightingShader);
+		pandaRocks.Draw(lightingShader);
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-		
 		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(posXLeones, posYLeones + 0.01f, posZLeones));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0.0);
+		glUniform4f(glGetUniformLocation(lightingShader.Program, "colorAlpha"), 1.0f, 1.0f, 1.0f, 0.9f);
+		lionWater.Draw(lightingShader);
+
+		/*model = glm::mat4(1);
 		model = glm::translate(model, glm::vec3(posX, posY, posZ));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0);
@@ -611,11 +650,34 @@ int main()
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0);
 		glUniform4f(glGetUniformLocation(lightingShader.Program, "colorAlpha"), 1.0f, 1.0f, 1.0f, 0.75f);
-		foco.Draw(lightingShader);
+		foco.Draw(lightingShader);*/
 
 		
 
 		glDisable(GL_BLEND);
+
+		//Load transparency models for panda habitat
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(posXPanda, posYPanda, posZPanda));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 1.0);
+		pandaBambuPlant.Draw(lightingShader);
+		model = glm::translate(model, glm::vec3(1.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		pandaBambuPlant.Draw(lightingShader);
+		model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		pandaBambuPlant.Draw(lightingShader);
+		model = glm::translate(model, glm::vec3(12.0f, 0.0f, -14.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		pandaBambuPlant.Draw(lightingShader);
+		model = glm::translate(model, glm::vec3(1.0f, 0.0f, -1.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		pandaBambuPlant.Draw(lightingShader);
+		model = glm::translate(model, glm::vec3(-2.0f, 0.0f, -1.0f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		pandaBambuPlant.Draw(lightingShader);
+
 		glEnable(GL_DEPTH_TEST);
 		glUniform4f(glGetUniformLocation(lightingShader.Program, "colorAlpha"), 1.0f, 1.0f, 1.0f, 1.0f);
 		glBindVertexArray(0);
